5ce26b2b97529cbb6b7cc626a35c0335f7e2a1c0
18062048550 <18062048550@163.com>
2017-08-28 06:16:29 +0800

    错误纠正
    change error

--- task/src/main/java/cn/weblog/task/serivce/impl/TaskSeriveImpl.java
+++ task/src/main/java/cn/weblog/task/serivce/impl/TaskSeriveImpl.java
@@ -1,5 +1,6 @@
 package cn.weblog.task.serivce.impl;
 
+import cn.weblog.task.BaseTask;
 import cn.weblog.task.mapper.TaskMapper;
 import cn.weblog.task.pojo.Task;
 import cn.weblog.task.serivce.TaskService;
@@ -11,23 +12,11 @@ public class TaskSeriveImpl implements TaskService {
 
     private TaskMapper taskMapper;
 
-    public List<Task> getTask() {
-
-        return taskMapper.getTask();
-
+    public List<BaseTask> getTask() {
+        return null;
     }
 
-    public List<Task> getTaskAndResult(Integer userId) {
-
-        List<Task> allTasks= this.getTask();
-
-        for (Task task : allTasks) {
-
-
-
-
-        }
-
+    public List<BaseTask> getTaskAndResult(Integer userId) {
         return null;
     }
 }
